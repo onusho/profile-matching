e20271c11ade334fb636dada4d58d50bebafa5e4
onusho <anush.verma247@gmail.com>
2025-02-15 19:18:07 +0530

    complete

--- app/app.py
+++ app/app.py
@@ -1,6 +1,3 @@
-###############################
-# app.py
-###############################
 from flask import Flask, render_template, request, redirect, url_for, flash
 from pymongo import MongoClient
 from joblib import load
@@ -11,9 +8,6 @@ from sklearn.preprocessing import MinMaxScaler
 import _pickle as pickle
 from random import sample
 
-# -------------------------------
-# 1. MongoDB Connection
-# -------------------------------
 client = MongoClient("mongodb://localhost:27017/")
 db = client["mydatabase"]
 
@@ -50,78 +44,42 @@ scaler = load(".\\data\\scaler.joblib")
 MIN_AGE = 18
 MAX_AGE = 100
 
-# -------------------------------
-# 2. Flask App Configuration
-# -------------------------------
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret_key"  
 
-
-# -------------------------------
-# 4. Helper Functions
-# -------------------------------
 def string_convert(x):
-    """Converts lists to space‐separated strings."""
     if isinstance(x, list):
         return ' '.join(x)
     return x
 
 def vectorization(df, columns, input_df):
-    """
-    Using recursion, iterate through the df until all the categories have been vectorized
-    """
 
     column_name = columns[0]
         
-    # Checking if the column name has been removed already
     if column_name not in ['Bios', 'Movies','Religion', 'Music', 'Politics', 'Social Media', 'Sports']:
                 
         return df, input_df
     
-    # Encoding columns with respective values
-    if column_name in ['Religion', 'Politics']:
-        
-        # Getting labels for the original df
+    if column_name in ['Religion', 'Politics']:        
         df[column_name.lower()] = df[column_name].cat.codes
-        
-        # Dictionary for the codes
         d = dict(enumerate(df[column_name].cat.categories))
-        
         d = {v: k for k, v in d.items()}
-                
-        # Getting labels for the input_df
         input_df[column_name.lower()] = d[input_df[column_name].iloc[0]]
-                
-        # Dropping the column names
         input_df = input_df.drop(column_name, axis=1)
-        
         df = df.drop(column_name, axis=1)
         
         return vectorization(df, df.columns, input_df)
     
-    # Vectorizing the other columns
     else:
-        # Instantiating the Vectorizer
-        vectorizer = CountVectorizer()
-        
-        # Fitting the vectorizer to the columns
+        vectorizer = CountVectorizer()        
         x = vectorizer.fit_transform(df[column_name].values.astype('U'))
-        
         y = vectorizer.transform(input_df[column_name].values.astype('U'))
-
-        # Creating a new DF that contains the vectorized words
         df_wrds = pd.DataFrame(x.toarray(), columns=vectorizer.get_feature_names_out())
-        
         y_wrds = pd.DataFrame(y.toarray(), columns=vectorizer.get_feature_names_out(), index=input_df.index)
-
-        # Concating the words DF with the original DF
         new_df = pd.concat([df, df_wrds], axis=1)
-        
         y_df = pd.concat([input_df, y_wrds], axis=1)
-
-        # Dropping the column because it is no longer needed in place of vectorization
         new_df = new_df.drop(column_name, axis=1)
-        
         y_df = y_df.drop(column_name, axis=1)
         
         return vectorization(new_df, new_df.columns, y_df) 
@@ -140,35 +98,18 @@ def scaling(df, input_df):
     return input_vect
 
 def top_ten(cluster, vect_df, input_vect):
-    """
-    Returns the DataFrame containing the top 10 similar profiles to the new data
-    """
-    # Filtering out the clustered DF
     des_cluster = vect_df[vect_df['cluster']==cluster[0]].drop('cluster', axis=1)
-    
-    # Appending the new profile data
     des_cluster = pd.concat([des_cluster, input_vect], ignore_index=False)
-            
-    # Finding the Top 10 similar or correlated users to the new user
     user_n = input_vect.index[0]
-    
-    # Trasnposing the DF so that we are correlating with the index(users) and finding the correlation
     corr = des_cluster.T.corrwith(des_cluster.loc[user_n])
-
-    # Creating a DF with the Top 10 most similar profiles
     top_10_sim = corr.sort_values(ascending=False)[1:11]
-        
-    # The Top Profiles
     top_10 = df.loc[top_10_sim.index]
-        
-    # Converting the floats to ints
     top_10[top_10.columns[1:]] = top_10[top_10.columns[1:]]
     
     return top_10.astype('object')
 
 
 def get_example_bios():
-    """Return 3 random example bios from df."""
     return sample(list(df['Bios']), 3)
 
 def get_data():
@@ -221,7 +162,7 @@ def get_data():
         if not valid:
             return redirect(url_for('index'))
         
-        flash('Form submitted successfully!', 'success')
+        # flash('Form submitted successfully!', 'success')
         return (bio, movies_list, religion, music_list, politics, social_media_list, sports_list, age)
     
 def get_profile():
@@ -252,12 +193,6 @@ def get_profile():
                                                 'Conservative'])
     return profile
 
-        
-
-    
-# -------------------------------
-# 5. Routes
-# -------------------------------
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -304,8 +239,5 @@ def show_profiles():
         profile["_id"] = str(profile["_id"])
     return render_template("profiles.html", profiles=all_profiles)
 
-###############################
-# 6. Main
-###############################
 if __name__ == "__main__":
     app.run(debug=True)
